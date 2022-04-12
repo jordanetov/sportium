492876bab252512cea6bf5bdb394a2cae9744f64
unknown <jordanetov@gmail.com>
2022-04-12 22:49:44 +0200

    UserAdmin

--- sportium/accounts/admin.py
+++ sportium/accounts/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 from sportium.accounts.models import Profile
+from sportium.common.helpers import UserModel
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     pass
     # list_display = ('first_name', 'last_name')
+
+
+@admin.register(UserModel)
+class UserAdmin(admin.ModelAdmin):
+    pass
